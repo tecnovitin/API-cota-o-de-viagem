3b82ea374b324b45b55c63b25c9b366e2aaf833a
tecnovitin <victor.160612-2023@aluno.unicv.edu.br>
2025-06-07 00:21:56 -0300

    Corrigido DTO

--- src/main/java/com/example/demo/dto/DescontoDTO.java
+++ src/main/java/com/example/demo/dto/DescontoDTO.java
@@ -1,9 +1,9 @@
 package com.example.demo.dto;
 
-import java.math.BigDecimal;
+
 import java.time.LocalDateTime;
 
-import jakarta.validation.constraints.DecimalMin;
+
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
